39d2f1f7131cb36e052277f9b8f605b8fcfe4261
Cynthia O'Donnell <mimibambino@gmail.com>
2015-01-14 11:14:00 +0100

    wikipedia data shows up

--- js/app.js
+++ js/app.js
@@ -702,34 +702,35 @@ var ViewModel = function() {
  				return function() {
  					infowindow.setContent("<div class='infoWindow'><h3>Hi, my name is " + marker.title + "!</h3></div>");
    					infowindow.open(map, marker);
-   					self.dinoDataRequest(marker);
+   					self.dinoDataRequest(marker, infowindow);
     				};
    				})(marker));
  		}
 	};
 
-	this.dinoDataRequest = function(marker){
+	this.dinoDataRequest = function(marker, infowindow){
     	var url = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=";
     	url += marker.title;
 
     	$.ajax( {
     		url: url,
-    		//data: queryData,
     		xhrFields: {
       			withCredentials: true
    			},
     		dataType:'jsonp',
     		success: function(response){
+    			var keys = Object.keys(response.query.pages);
+    			var key = parseInt(keys[0], 10);
+    			var paragraph = response.query.pages[key].extract;
     			console.log(response);
+    			infowindow.setContent("<div class='infoWindow'><h3>Hi, my name is " + marker.title + "!</h3></div><div>" + paragraph + "</div>");
     		},
     		type:'GET',
     		headers: { 
     			'Api-User-Agent': "Cynthia O\'Donnell: mimibambino@gmail.com",
     			'Access-Control-Allow-Origin': true
     		 }
-			} ).done(function() {
-				infowindow.setContent("Ajax returned");
-			});
+			} );
     };
 
 	this.init = function() {
